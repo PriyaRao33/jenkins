3b02f1534b953179e9755509bce56352fb416bf0
root <root@ip-172-31-29-96.ec2.internal>
2022-11-03 07:55:28 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+region= "us-east-1"
+access_key="AKIAQH5UHF36W33AJVMY"
+secret_key= "XZnBqjoQyjWjnajNm0cjMUDNppabJQjsmAvy1UF2"
+}
+resource "aws_instance" "example" {
+ami= "ami-08c40ec9ead489470"
+instance_type= var.instance_type
+tags= {
+name= "priya"
+}
+}
+resource "aws_s3_bucket"   "web" {
+bucket= "maha0099"
+}
+variable "instance_type" {
+description= "instance type t2.micro"
+type=string
+default="t2.micro"
+}
