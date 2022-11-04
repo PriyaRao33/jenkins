9aa35885af8ee510f6706b24bf8b9437d2ccd9be
PriyaRao33 <101392817+PriyaRao33@users.noreply.github.com>
2022-11-04 13:34:07 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,12 +1,7 @@
 resource "aws_instance"   "web" {
   ami= "ami-09d3b3274b6c5d4aa"
-  instance_type= var.instance_type
+  instance_type= "t2.micro"
   tag= {
     name= "priya"
   }
-}
-variable "instance_type" {
-  description= "instance type t2.micro"
-  type= string
-  default= "t2.micro"
 }
