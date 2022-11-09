19b08e3a9536b0af31fb2d213051d209f7946ed1
PriyaRao33 <101392817+PriyaRao33@users.noreply.github.com>
2022-11-09 14:54:24 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -3,11 +3,5 @@ provider "aws" {
   access_key = "AKIAQH5UHF3672PLYEGT"
   secret_key = "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"
 }
-resource "aws_instance"  "webee" {
-  ami = "ami-09d3b3274b6c5d4aa"
-  instance_type = "t2.micro"
-  tags = {
-    name = "priya"
-  }
-}
+
 
